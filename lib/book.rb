4f439f882d0afb2638f3bf9640b5759d2e4eddbf
Teanna <teannamtaylor95@gmail.com>
2023-05-07 13:35:06 -0700

    commit tree c597be0c578cb969bd421d9b14c3f66bdcfe92a3

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+    attr_accessor :title
+    attr_reader :author, :page_count, :genre
+    
+    def initialize(title)
+        @title = title
+    end
+ 
+    def author=(author)
+        @author = author
+    end
 
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
 # Make your shoe class here!
+
+class Shoe 
+    attr_accessor :brand
+    attr_reader :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+    def size=(size)
+        @size = size
+    end
+    def material=(material)
+        @material = material
+    end
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def cobble 
+        if @condition != "new"
+            puts "Your shoe is as good as new!"
+            @condition = "new"
+        end 
+    end
+
+end
